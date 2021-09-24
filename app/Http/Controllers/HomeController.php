9f3b27f8dac926b060a0cf9f0564b11b1d6b49b4
jihadabdulrazaqjasim <jihad.abdulrazaq@gmail.com>
2021-09-25 02:40:59 +0300

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -167,7 +167,7 @@ class HomeController extends Controller
                     $output .= str_replace('+',' ',$index['index']);
 
                     $output .= "<td>";
-                    $output.= "<a class='delete' href='#' style='color:red;text-decoration:underline'  index=".$index['index'].">Delete</a>";                    
+                    $output.= " <form action='POST'><a class='delete' href='#' style='color:red;text-decoration:underline'  index=".$index['index'].">Delete</a></form>";                    
                     $output.="</td>";
                     $output.="</tr>";
                 }
